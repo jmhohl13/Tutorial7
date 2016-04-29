49c969c3ac31d5cb26206d734462def86a227b14
jmhohl13 <hohljm@rose-hulman.edu>
2016-04-29 10:29:46 -0400

    PawnMovement "one space forward" passes

---
+++ ChessBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial7
+{
+    public class Chessboard
+    {
+        Dictionary<Tuple<char,int>, Pawn> pawns = new Dictionary<Tuple<char, int>, Pawn>();
+
+
+        public void placePiece(Pawn p, Tuple<char,int> loc )
+        {
+            pawns.Add(loc,p);
+        }
+
+        public void movePiece(Tuple<char,int> old, Tuple<char,int> next)
+        {
+            Pawn temp = pawns[old];
+            pawns.Remove(old);
+            pawns.Add(next, temp);
+        }
+    }
+}
---
+++ Pawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial7
+{
+    public class Pawn
+    {
+        public Pawn(String color)
+        {
+            
+        }
+    }
+}
